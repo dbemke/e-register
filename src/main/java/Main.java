10474173ae33ab820c5e7fd6d9a0dafdd13f25d0
Dominika Bemke <dominikabemke@gmail.com>
2022-04-19 12:58:07 +0200

    Add mariadb connector.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        String url = "jdbc:mariadb://localhost";
+        String user = "root";
+        String pass = "";
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, pass);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
     }
 }
